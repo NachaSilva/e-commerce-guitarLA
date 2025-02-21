7045f3d79f57032506b315736023c9dc4eceaef6
Ignacia Silva <ignaciasilva1@gmail.com>
2025-02-21 11:12:16 -0300

    Create useCart Hook to move all the functionalities focus on cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,10 @@
-import {useState, useEffect} from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { db } from './data/db'
+import { useCart } from './hooks/useCart'
 
 function App() {
-
-  const initialCart= ()=>{
-    const localStorageCart = localStorage.getItem('cart')
-    return localStorageCart ? JSON.parse(localStorageCart) : []
-  }
-
-  const [data] = useState(db)
-  const [cart, setCart]=useState(initialCart())
-
-  const MAX_ITEMS =5
-  const MIN_ITEMS =1
-
-  useEffect(()=>{
-    localStorage.setItem('cart', JSON.stringify(cart))
-  },[cart])
-
-  function addToCart(item){
-    const itemExists= cart.findIndex((guitar)=>guitar.id === item.id)
-   if(itemExists >= 0 ){
-    if(cart[itemExists].quantity>=MAX_ITEMS)return //existe en el carrito
-   const updatedCart = [...cart]
-   updatedCart[itemExists].quantity++
-   setCart(updatedCart)
-   }else{
-    item.quantity = 1
-    setCart([...cart, item])
-   }
-
-  }
-
-  function removeFromCart(id){
-  setCart(prevCart => prevCart.filter(guitar=>guitar.id !== id))
-  }
-
-  function increaseQuantity(id){
-    const updatedCart= cart.map(item=>{
-      if(item.id===id && item.quantity < MAX_ITEMS){
-        return{
-          ...item,
-          quantity: item.quantity+1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function decreaseQuantity(id){
-    const updatedCart= cart.map(item=>{
-      if(item.id===id && item.quantity > MIN_ITEMS){
-        return{
-          ...item,
-          quantity: item.quantity-1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function clearCart(){
-    setCart([])
-  }
-
+const { data,cart,addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty,cartTotal}= useCart()
+  
   return (
     <>
     <Header
@@ -76,6 +13,8 @@ function App() {
     increaseQuantity={increaseQuantity}
     decreaseQuantity = {decreaseQuantity}
     clearCart = {clearCart}
+    isEmpty={isEmpty}
+    cartTotal= {cartTotal}
     />
     
 
@@ -87,7 +26,6 @@ function App() {
                <Guitar
                key={guitar.id}
                guitar = {guitar}
-               setCart={setCart}
                addToCart={addToCart}
                />
           )  
@@ -95,7 +33,6 @@ function App() {
          
         </div>
     </main>
-
 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
